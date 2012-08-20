e3cbf7fa38cce378fcdc375ccd79bb71e43229ad
Adam Christian <adam.christian@gmail.com>
2012-08-20 00:12:24 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# NWA -- Node Web Apps, quick starter
+
+## Authors
+
+  - Adam Christian ([admc](http://github.com/admc))
+  
+## License
+
+  * License - Apache 2: http://www.apache.org/licenses/LICENSE-2.0
+
+## Usage
+
+<pre>
+sh nwa.sh 'appname'
+</pre>
+
+
+## Then what
+
+<pre>
+cd 'appname'
+node app.js
+</pre>
+
+## Tool Choices
+
+ - [expressjs](http://expressjs.com/)
+  - [less](http://lesscss.org/)
+  - [ejs](http://embeddedjs.com/)  
+ - [socket.io](http://socket.io/)
+ - [jquery](http://jquery.com/)
+ - [jquery-ui](http://jqueryui.com/)
+ - [video-js](http://videojs.com/)
+ - [audiojs](http://kolber.github.com/audiojs/)
+ - [pouchdb](http://pouchdb.com/)
---
+++ nwa.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+if [ "$1" == "" ]; then
+  echo "\n\033[0;31mPlease provide a web app name!!!!\033[0m\n";
+  exit 1;
+else
+  echo "\n\033[0;36mSetting up your awesome new webapp, $1\033[0m...\n";
+fi
+
+npm install socket.io express
+express --sessions --css less --ejs $1
+cd $1 && npm install
+
+cd public/javascripts
+curl -O http://code.jquery.com/jquery-1.8.0.min.js
+curl -O http://cloud.github.com/downloads/daleharvey/pouchdb/pouch.alpha.min.js
+
+cd ..
+curl -O http://cloud.github.com/downloads/zencoder/video-js/video-js-3.2.0.zip
+unzip video-js-3.2.0.zip
+
+curl -O http://kolber.github.com/audiojs/audiojs.zip
+unzip audiojs.zip
+
+mkdir jquery-ui
+cd jquery-ui
+curl -O http://jqueryui.com/download/jquery-ui-1.8.23.custom.zip
+unzip jquery-ui-1.8.23.custom.zip
+
+cd ..
+cd ..
+cd views
+rm index.ejs
+
+echo '<!DOCTYPE html>
+<html>
+  <head>
+    <title><%= title %></title>
+    <link rel="stylesheet" href="/stylesheets/style.css" />
+    <link rel="stylesheet" href="/video-js/video-js.css" />
+    <link rel="stylesheet" href="/jquery-ui/css/smoothness/jquery-ui-1.8.23.custom.css" />
+    <script src="/javascripts/jquery-1.8.0.min.js"></script>
+    <script src="/javascripts/pouch.alpha.min.js"></script>
+    <script src="/jquery-ui/js/jquery-ui-1.8.23.custom.min.js"></script>
+    <script src="/video-js/video.js"></script>
+    <script src="/audiojs/audio.min.js"></script>
+    <script src="/socket.io/socket.io.js"></script>
+    <script>
+      var socket = io.connect("http://localhost");
+      socket.on("news", function (data) {
+        console.log(data);
+        socket.emit("my other event", { my: "data" });
+      });
+    </script>
+  </head>
+  <body>
+    <h1><%= title %></h1>
+    <p>Welcome to <%= title %></p>
+  </body>
+</html>' > index.ejs
+
+cd ..
+rm app.js
+
+echo "
+var express = require('express')
+  , app = express()
+  , http = require('http')
+  , server = http.createServer(app)
+  , io = require('socket.io').listen(server)
+  , routes = require('./routes')
+  , path = require('path')
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
+  app.use(app.router);
+  app.use(require('stylus').middleware(__dirname + '/public'));
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});" > app.js
+
+echo "\n\033[0;32mWe have done all the things, starting your '$1' server! \033[0m\n";
+echo "\n\033[0;32mAfter you kill this process, you can start your app again with 'node app.js'.\033[0m\n";
+
+node app.js
